0625f81abb6a911dee9ccb184df23b40dcb028de
rdjara <jararoyjrd@gmail.com>
2024-05-28 18:45:54 +0800

    Module Changes: 1. Added a function for serialized dla
    Notebook changes:
    1. created a sample plot of the cluster created using the serialized dla function

--- Julia_serialized_DLA/serialized_dla_modules.jl
+++ Julia_serialized_DLA/serialized_dla_modules.jl
@@ -1,6 +1,6 @@
-#creating object for a random walker
-
 module Random_walker
+
+
 
 #function for creating a random walker
 function initialize_randomwalker(birth_radius::Int64)
@@ -26,7 +26,7 @@ function walker_update_position(walker::Array, death_radius::Int64, birth_radius
     
 end
 
-
+#sample function for generating a random walk
 function random_walk_generator(step_number::Int64, death_radius::Int64, birth_radius::Int64)
     walker_trajectory = zeros(Int64, (2, step_number))
     walker_position = initialize_randomwalker(birth_radius)
@@ -38,6 +38,66 @@ function random_walk_generator(step_number::Int64, death_radius::Int64, birth_ra
 end
 
 
+
+
+function serialized_dla(starting_birth_radius::Int64, starting_death_radius::Int64, particle_number::Int64)
+    cluster_aggregate = zeros(Int64, (2, particle_number + 1))
+
+
+    #variables to be dynamically updated
+    cluster_particle_number = 1
+    birth_radius = starting_birth_radius
+    death_radius = starting_death_radius
+
+
+    #creating the cluster
+    for particle in 1:particle_number
+        walker_position = initialize_randomwalker(birth_radius)
+        far_from_cluster = true
+
+        #random walk of a single particle
+        while far_from_cluster
+            for i in 1 : cluster_particle_number
+                distance_from_cluster_particle = sum((cluster_aggregate[:, i] - walker_position).^2)
+                if abs(distance_from_cluster_particle - 1) < 1e-6
+                    cluster_particle_number += 1
+
+                    cluster_aggregate[:, cluster_particle_number] = walker_position
+                    far_from_cluster = false
+                    break
+                end
+            end 
+            walker_position = walker_update_position(walker_position, death_radius, birth_radius)
+        end
+
+
+        #updating birth and death birth_radius
+        cluster_particle_distance_array = zeros(Float64, cluster_particle_number)
+        for i in 1 : cluster_particle_number
+            cluster_particle_distance_array[i] = sum(cluster_aggregate[:, i].^2)
+        end
+
+        if maximum(cluster_particle_distance_array) > birth_radius^2
+            birth_radius += 1
+            if birth_radius^2 > 0.5 * death_radius^2
+                death_radius += 10
+            end
+        end
+
+        print("\r Walker # $particle done!")
+    end
+
+    return cluster_aggregate 
+end
+
+
 end #end of Random_walker module
 
 
+# using ..Random_walker
+
+# const starting_birth_radius = 5
+# const starting_death_radiua = 10
+# const particle_number = 3
+
+# println(Random_walker.serialized_dla(starting_birth_radius, starting_death_radiua, particle_number))
